a3c26ea33cc2d346859ea3d0769afef5e9e69eea
milshumskiy <38204155+milshumskiy@users.noreply.github.com>
2020-11-27 13:59:06 +0300

    Update make_watermark_on_photo.py

--- make_watermark_on_photo.py
+++ make_watermark_on_photo.py
@@ -1,8 +1,11 @@
 from PIL import Image
-from main import save_edited_image
-from main import add_to_edited_table
+from save_instruments import save_edited_image
+from save_instrumens import add_to_edited_table
 
 def watermark_photo(input_image_path, watermark_image_path, position):
     base_image = Image.open(input_image_path)
     watermark = Image.open(watermark_image_path)
 
+    # add watermark to image
+    if (base_image.size > watermark.size):
+        base_image.paste(watermark, position)
